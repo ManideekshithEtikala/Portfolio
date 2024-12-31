08432935fd7708eb804c5947d15ef5d8da8482b0
manideekshith <manideekshithgaming@gmail.com>
2024-12-31 23:49:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-200 selection:text-black">
+    <Navbar/>
+    </div>
+    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    </>
+  );
+}
+
+export default App;
