f1ae5ce3713255d2d57c36dfca02724e20a685bb
manideekshith <manideekshithgaming@gmail.com>
2025-01-01 14:26:55 +0530

    this is about me completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import "./App.css";
 import About from "./components/About";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import Technologies from "./components/Technologies";
 
 function App() {
   return (
     <>
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-200 selection:text-black">
-    <Navbar/>
-    <Main/>
-    <About/>
-    </div>
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-200 selection:text-black">
+        <div className="fixed top-0 -z-10 h-full w-full">
+
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+        </div>
+        <div className="container mx-auto px-8">
+
+        <Navbar />
+        <Main />
+        <About />
+        <Technologies/>
+        </div>
+      </div>
+
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import aboutImg from "../assets/about.jpeg";
+import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4xl">
+        About
+        <span className="text-neutral-500">Me</span>
+      </h1>
+      <div className="flex flex-col md:flex-row items-center lg:items-start " >
+        <div className="w-full lg:w-1/2 lg:p-8 ">
+          <div className="flex items-center justify-center ">
+          <img
+            src={aboutImg}
+            className="rounded-2xl h-60 md:h-96 lg:h-[24rem] hover:shadow-xl hover:shadow-white/50 transition-shadow duration-300 hover:ease-in-out" 
+            alt="aboutmeimage"
+          />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-5">{ABOUT_TEXT}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
